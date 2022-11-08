6474a37e35062cd5f68e358521a9b8c785b6e825
Yash Kumar <its.yash.kumar23@gmail.com>
2022-11-08 23:56:13 +0530

    Add files via upload

---
+++ first_last_Occurence.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int firstOccurence(vector<int> arr, int idx, int target){
+
+    if(idx == arr.size()){
+        return -1;
+    }
+
+    
+    if( arr[idx] == target ){
+        return idx;
+    }
+    else{
+        int fiisa = firstOccurence(arr, idx + 1, target);
+        return fiisa;
+    }
+
+}
+
+int lastOccurence(vector<int> arr, int idx, int target){
+
+    if(idx > arr.size()){
+        return -1;
+    }
+
+    int liisa = lastOccurence(arr, idx + 1, target);
+    if(liisa == -1){
+        if(arr[idx] == target){
+            return idx;
+        }
+        else{
+            return -1; 
+        }
+    }
+    else{
+        return liisa;
+    }
+}
+
+int main(){
+    
+
+    vector<int> arr = {3,7,8,6,1,3,8,8,4,1};
+    cout<<firstOccurence(arr, 0, 8)<<endl;
+    cout<<lastOccurence(arr, 0, 3)<<endl;
+
+    return 0;
+}
